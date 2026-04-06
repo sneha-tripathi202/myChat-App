c6ccdde5b22fb43b3b29aed7f2bfa5a30f6816e4
Sneha Tripathi <st0744397@gmail.com>
2026-04-06 12:32:55 +0530

    Login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import SignUp from './pages/SignUp'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<SignUp/>
-          
+{/* <SignUp/> */}
+<LoginPage/>          
     </>
   )
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,25 +1,33 @@
 import React from 'react'
 
-const SignUp = () => {
+const LoginPage = () => {
   return (
-    <div className='w-screen h-screen bg-radial-[at_70%_75%] from-blue-800 via-gray-900 to-black to-90% flex item-center   justify-center '>
+       <div className='w-screen h-screen bg-radial-[at_70%_75%] from-blue-800 via-gray-900 to-black to-90% flex item-center   justify-center '>
       <div className='bg-black/30 my-30 sm:w-100 sm:h-118 shadow-2xl shadow-blue-950 rounded-xl flex flex-col items-center py-6 text-white'>
-        <h1 className='text-2xl font-bold py-2'>Create an Account</h1>
-        <h4 className='text-mist-300'>Sign up to get started</h4>
+        <h1 className='text-2xl font-bold py-2'>Welcome back!</h1>
+        <h4 className='text-mist-300'>Login to your account</h4>
 
         <div className='mt-6 mb-4 border-y border-t-gray-700 border-b-gray-500 sm:w-80  '>
           <form action="" className=' my-3 flex flex-col'>
-            <input className='bg-white/30 p-2 m-1 mt-2 rounded placeholder-gray-300 ' type="text" placeholder='Full Name' />
           <input className='bg-white/30 p-2 m-1 rounded placeholder-gray-300' type="text" placeholder='Email' />
           <input className='bg-white/30 p-2 m-1 rounded placeholder-gray-300' type="text" placeholder='Passowrd' />
-          <input className='bg-white/30 p-2 m-1 rounded placeholder-gray-300' type="text" placeholder='Confirm Password' />
+         <div className='flex justify-between m-3'>
+           <div> <input  type="checkbox" />
+           <label className="ml-2 text-gray-200">Remember me</label>
+</div>
+           <h1 className=' text-mist-400 underline'>Forgot Password?</h1>
+         </div>
+         
           <button className='mt-6 mb-4 p-1 text-[0.7] font-semibold mx-4 rounded sm:w-70 bg-linear-to-l from-blue-700 via-blue-500 to-blue-900'>Sign up</button></form>
         </div>
         
-        <div > <h3 className='mb-2 text-mist-100'>Already have an account? Login</h3></div>
+        <div > <h3 className='mb-2 text-mist-100'>Don't have an account? Login</h3></div>
       </div>
+
+
+
     </div>
   )
 }
 
-export default SignUp
+export default LoginPage
