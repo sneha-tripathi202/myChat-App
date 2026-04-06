d752b80b819a6ff078ad6f9182d3ae207db9cd07
Sneha Tripathi <st0744397@gmail.com>
2026-04-06 23:15:24 +0530

    .

--- src/Components/People.jsx
+++ src/Components/People.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const People = () => {
   return (
-    <div className=''>
+    <div className='sm:'>
       <div><img src="" alt="" />
            <h1>Sneha</h1>
-           <div><span></span><h1>Online</h1></div></div>
+           <div><span>.</span><h1>Online</h1></div></div>
       <div></div>
     </div>
   )
